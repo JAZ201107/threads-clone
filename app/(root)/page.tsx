3826ab3e0cb209470ed4303358e9ff7f24aadd2c
zhang yuyang <zhangyuyang1211@gmail.com>
2023-08-06 19:11:41 +0800

    Homepage Finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className="underline">
-      Root <UserButton afterSignOutUrl="/" />
+      <h1 className="head-text text-left">Home</h1>
     </div>
   );
 }
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+
+interface RightSidebarProps {}
+
+const RightSidebar: FC<RightSidebarProps> = ({}) => {
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-col justify-start flex-1">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Communities
+        </h3>
+      </div>
+
+      <div className="flex flex-col justify-start flex-1">
+        <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
