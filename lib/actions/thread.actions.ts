077581663edc893345ba9fe44bb9dea873a8539d
zhang yuyang <zhangyuyang1211@gmail.com>
2023-08-07 22:49:33 +0800

    Fetch Posts

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -44,3 +44,41 @@ export async function createThread({
     throw new Error(`Failed to create thread: ${error.message}`);
   }
 }
+
+export async function fetchPost(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  const postQuery = Thread.find({
+    parentId: { $in: [null, undefined] },
+  })
+    .sort({
+      createdAt: "desc",
+    })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: "author",
+      model: User,
+    })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
